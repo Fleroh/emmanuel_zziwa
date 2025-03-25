7e019fcddd471d740270a4baf0ccaa7048e6831d
Emmah fleroh <emmahzziwa@gmail.com>
2025-03-25 11:09:52 +0300

    Add files via upload

---
+++ ZZIWA EMMAH.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int balance = 50000;
+    int withdrawal = 20000;
+
+    
+    if (withdrawal > balance) {
+        printf("Insufficient funds. Your balance is %d\n", balance);
+    } else {
+        
+        balance -= withdrawal;
+        printf("You have withdrawn %d\n", withdrawal);
+        printf("Your new account balance is %d\n", balance);
+    }
+
+    return 0;
+}
